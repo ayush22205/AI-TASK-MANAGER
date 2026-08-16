bb37abd8be91f7faf07be088c1b6ea0fa850d986
ayush22205 <ayushk20708090@gmail.com>
2026-08-16 12:36:18 +0530

    added Docker support - Dockerfile and docker-compose

--- Fastapi/database.py
+++ Fastapi/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import  create_engine
 from sqlalchemy.orm import declarative_base , sessionmaker 
 from datetime import datetime
 
-URL = "postgresql://postgres:ayush123@localhost:5432/taskmanager"
+URL = "postgresql://postgres:ayush123@db:5432/taskmanager"
 
 engine = create_engine(URL)
 
